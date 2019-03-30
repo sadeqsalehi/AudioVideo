b1a74ba47d7d24d1ae4aa8ff6f82a935383bcebe
sadeqsalehi <sadeqsalehi@gmail.com>
2019-03-30 09:15:43 +0430

    Play Video and add Controller

--- app/src/main/java/ir/sadeqsalehi/audiovideo/MainActivity.java
+++ app/src/main/java/ir/sadeqsalehi/audiovideo/MainActivity.java
@@ -2,12 +2,28 @@ package ir.sadeqsalehi.audiovideo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
+    VideoView videoView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        videoView = findViewById(R.id.videoPlayer);
+    }
+
+    public void playVideoOnClick(View view) {
+        String path="android.resource://"+getPackageName()+"/"+R.raw.big_buck_bunny_360p_5mb;
+        videoView.setVideoPath(path);
+        MediaController mediaController = new MediaController(this);
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
+        mediaController.show();
     }
 }
